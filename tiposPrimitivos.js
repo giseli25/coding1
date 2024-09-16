b4398797c7cb0018b8a32beab1c45c760f7b6c8f
Giseli Felix dos Santos <giseli2025@gmail.com>
2024-09-16 23:00:15 +0000

    ataulizando codigo

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -1,1 +1,17 @@
+//este codigo foi para treino de variáveis
 
+/*const rotulo="natalya"
+let rotulo=12345;
+console.log(rotulo)
+
+
+myName = "Lindalva
+";
+console.log( 'hello' +' '+ myName);*/
+
+const numero =25
+const numero1 = 24
+const soma = numero + numero1
+const subtracao = numero-numero1
+console.log( subtracao)
+console.log( soma )
